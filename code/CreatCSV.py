b5ddd5beb70e08b9d2c6aab35c0764aca5bf59a2
andy <laiguanhua@laiguanhuadeMacBook-Air.local>
2025-01-03 12:00:53 +0800

    Add CreateCSV.py

--- code/CreatCSV.py
+++ code/CreatCSV.py
@@ -1,14 +1,17 @@
 import csv
+import os
 
 class CreatCSV:
     def __init__(self, data):
+
         self.csv_path = '../output_csv/score.csv'
         # 將字典轉換為字典列表
         self.csv_data = [{'student_id': student_id, 'score': score} for student_id, score in data.items()]
         self.title = ['student_id', 'score']
+        if not os.path.exists(self.csv_path):
+            self.csv_path = '..\\output_csv\\score.csv'
 
     def creat_csv(self):  
-        print(self.csv_data)
         with open(self.csv_path, "w", newline="") as f:
             writer = csv.DictWriter(f, fieldnames=self.title)
             writer.writeheader()  # 寫入標題
